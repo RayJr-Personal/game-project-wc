593bc90fb060c26aaee808dd14f3318b1786c6cd
MageBruh <raymondstevejr@yahoo.com>
2021-09-16 19:00:06 +1200

    Updated gamefiles
    Updated version:
    - Implemented all steps mentioned from the previous update: 
         - Countdown timer at the start of the level
         - The 'finish line' feature and the 'timer' feature
         - The game over scenarios of crashing and not reaching the goal in time
         - Displaying an appropriate message for when the player wins, loses, or picks up an effect (effects not added yet).
    
    
    Next steps:
    - Implementing the three effects: Extra Boost, Slow Down, and Extra Time.
    - Adding Level 2 (either less time to reach the goal, or the goal distance is much larger).
    - Adding a main menu scene and a Game Over scene.
    
    No noticeable bugs for now, though it is possible that there is a bug with how NPC cars are being spawned.

--- gamefiles/Player.gd
+++ gamefiles/Player.gd
@@ -6,6 +6,8 @@ var boost = 100
 # General variables
 var extents
 var screensize
+var direction
+var game_over = false
 
 # Labels
 var label_boost
@@ -13,25 +15,42 @@ var label_boost
 # Signals
 signal boost_started
 signal boost_stopped
+signal game_over_signal
+
+
+# Checks whether the player has collided
+func collided(state):
+	if (state != null):
+		game_over = true
+		emit_signal("game_over_signal", "You crashed!")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label_boost = get_parent().get_node("Label")
+	label_boost = get_parent().get_node("BoostText")
 	
 	screensize = get_viewport_rect().size
 	extents = get_node("Player").get_texture().get_size() / 2
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	self.position.x = clamp(self.position.x, extents.x - 50, screensize.x - extents.x/2 - 25)
-	self.position.y = clamp(self.position.y, 190 + extents.y, screensize.y - extents.y/2)
+	pass
+
 
 func _physics_process(delta):
 	var input = Vector2()
-	input.x += int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	input.y += int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
-	var direction = input.normalized()
-	move_and_collide(direction * MOVE_SPEED * delta)
+	if (game_over == false):
+		input.x += int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+		input.y += int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+		self.position.x = clamp(self.position.x, extents.x - 50, screensize.x - extents.x/2 - 25)
+		self.position.y = clamp(self.position.y, 190 + extents.y, screensize.y - extents.y/2)
+	else:
+		input = Vector2(-1, 0)
+		
+	direction = input.normalized()
+	collided(move_and_collide(direction * MOVE_SPEED * delta))
+	
 	
 	if (Input.is_action_pressed("speed_up") and boost >= 0):
 		boost -= delta*50
--- gamefiles/RoadScroll.gd
+++ gamefiles/RoadScroll.gd
@@ -7,20 +7,24 @@ const ROAD_REPEAT_X = -167
 # (in pixels) width of white line + gap between 2 white lines
 const SCROLL_SPEED = 16.7
 
-var speed_multiplier
-var boost = 100
+var player
+var speed_multiplier = 0.75
 var roadPosition = Vector2()
+
+func _on_boost_started():
+	speed_multiplier = 2.2
+
+func _on_boost_stopped():
+	speed_multiplier = 0.75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = get_parent().get_node("KinematicBody2D")
+	player.connect("boost_started", self, "_on_boost_started")
+	player.connect("boost_stopped", self, "_on_boost_stopped")
 	roadPosition = get_offset()
 
 func _process(delta):
-	speed_multiplier = 0.75
-	 
-	if (Input.is_action_pressed("speed_up") and boost >= 0):
-		speed_multiplier = 2.2
-		boost -= delta*50
 	
 	roadPosition.x -= (SCROLL_SPEED * speed_multiplier)
 	if (roadPosition.x <= ROAD_REPEAT_X):
